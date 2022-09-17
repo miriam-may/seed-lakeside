0af19db0849c598c6ed9476e262457c29a986d20
Miriam Smith <mimsy.smith@yahoo.com>
2022-09-17 12:28:51 +1000

    made same changes to oter beds

--- Scripts/bed-29.js
+++ Scripts/bed-29.js
@@ -5,9 +5,9 @@ let beds = document.getElementById('twentynine');
 let bedheight = beds.clientHeight;
 let bedwidth = beds.clientWidth;
 
-celery.style.height = (bedheight/10) + 'px';
-celery.style.top = (bedheight/2.5) + 'px';
-celery.style.width = (bedwidth/20) + 'px';
+
+celery.style.top = (bedheight/10) + 'px';
+
 celery.style.left = -(bedwidth/100) +'px';
 
 
