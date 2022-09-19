b9c2804149039829ab0de1f5a6acb3b638eb0f4a
Miriam Smith <mimsy.smith@yahoo.com>
2022-09-19 10:55:19 +1000

    changes for mobile devices

--- Scripts/bed-26.js
+++ Scripts/bed-26.js
@@ -12,25 +12,71 @@ let rainbow_text = document.getElementById('rainbow_text');
 let lettuce = document.getElementById('lettuce');
 let lettuce_text = document.getElementById('lettuce_text');
 let beds = document.getElementById('twentysix')
-
+let imag26 = document.getElementById('imag-26');
 let bedheight = beds.clientHeight;
 let bedwidth = beds.clientWidth;
 x = 15
 y = 11.5
 
-//approx ratio of 15(w) to 11.5(h)
-x_a = bedwidth/x; //the amount of x's in width
-y_a = bedheight/y; //the amount of y's in height
+if(window.innerWidth < 580) {
+    
+    let iwidth = 370;
+    let iheight = imag26.clientHeight;
+   
+    rainbow.style.height = (iwidth/10) + 'px';
+    rainbow.style.top = (iheight/2.5) + 'px';
+    rainbow.style.width = (iwidth/10) + 'px';
+    rainbow.style.left = -(iwidth/1.9) +'px';
 
-rainbow.style.height = (bedheight/10) + 'px';
-rainbow.style.top = (y_a *(4.5)) + 'px';
-rainbow.style.width = (bedwidth/20) + 'px';
-rainbow.style.left = -(x_a *(0.1)) +'px';
+    lettuce.style.height = (iwidth/10) + 'px';
+    lettuce.style.top = (iheight/1.3) + 'px';
+    lettuce.style.width = (iwidth/10) + 'px';
+    lettuce.style.left = -(iwidth/4) +'px';
 
-lettuce.style.height = (bedheight/10) + 'px';
-lettuce.style.top = (y_a *(8)) + 'px';
-lettuce.style.width = (bedwidth/20) + 'px';
-lettuce.style.left = (x_a *(2.9)) +'px';
+    } 
+
+       else {
+
+
+
+    //approx ratio of 15(w) to 11.5(h)
+    x_a = bedwidth/x; //the amount of x's in width
+    y_a = bedheight/y; //the amount of y's in height
+
+    rainbow.style.height = (bedheight/10) + 'px';
+    rainbow.style.top = (y_a *(4.5)) + 'px';
+    rainbow.style.width = (bedwidth/20) + 'px';
+    rainbow.style.left = -(x_a *(0.1)) +'px';
+
+    lettuce.style.height = (bedheight/10) + 'px';
+    lettuce.style.top = (y_a *(8)) + 'px';
+    lettuce.style.width = (bedwidth/20) + 'px';
+    lettuce.style.left = (x_a *(2.9)) +'px';
+
+    function wresize(){
+        let bedheight = beds.clientHeight;
+        let bedwidth = beds.clientWidth;
+       
+        x = 15
+        y = 11.5
+       
+        //approx ratio of 15(w) to 11.5(h)
+        x_a = bedwidth/x; //the amount of x's in width
+        y_a = bedheight/y; //the amount of y's in height
+
+        rainbow.style.height = (bedheight/10) + 'px';
+        rainbow.style.top = (y_a *(4.5)) + 'px';
+        rainbow.style.width = (bedwidth/20) + 'px';
+        rainbow.style.left = -(x_a *(0.1)) +'px';
+
+        lettuce.style.height = (bedheight/10) + 'px';
+        lettuce.style.top = (y_a *(8)) + 'px';
+        lettuce.style.width = (bedwidth/20) + 'px';
+        lettuce.style.left = (x_a *(2.9)) +'px';
+}
+
+window.onresize = wresize;
+       }
 
 function revealRainbow(){
     if(lettuce_text.style.display="block"){
@@ -70,25 +116,3 @@ function revealLettuce(){
 rainbow.onclick = revealRainbow, true;
 lettuce.onclick = revealLettuce, true;
 
-function wresize(){
-    let bedheight = beds.clientHeight;
-    let bedwidth = beds.clientWidth;
-    x = 15
-    y = 11.5
-
-    //approx ratio of 15(w) to 11.5(h)
-    x_a = bedwidth/x; //the amount of x's in width
-    y_a = bedheight/y; //the amount of y's in height
-
-    rainbow.style.height = (bedheight/10) + 'px';
-    rainbow.style.top = (y_a *(4.5)) + 'px';
-    rainbow.style.width = (bedwidth/20) + 'px';
-    rainbow.style.left = -(x_a *(0.1)) +'px';
-
-    lettuce.style.height = (bedheight/10) + 'px';
-    lettuce.style.top = (y_a *(8)) + 'px';
-    lettuce.style.width = (bedwidth/20) + 'px';
-    lettuce.style.left = (x_a *(2.9)) +'px';
-}
-
-window.onresize = wresize;
