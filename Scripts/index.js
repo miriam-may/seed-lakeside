b40223b3a268c48d3408be4a0355f2fc64a11489
Miriam Smith <mimsy.smith@yahoo.com>
2023-02-17 09:22:54 +1100

    Initial changes for summer

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,7 +2,7 @@ window.onload = function(){
     let banner = document.getElementById('home_banner')
     banner.animate([
         {opacity: 0, color: '#000'},
-        {opacity: 1, color: '#08b'}
+        {opacity: 1, color: 'rgb(187, 168, 0)'}
     ], {
         duration: 2000,
         iterations: 1
